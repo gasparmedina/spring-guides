58a352457927c028e0b2f895aa37cd4dd5034bc1
gasparmedina <gasweb456@gmail.com>
2022-12-18 18:34:21 -0300

    [ADD] initial for building restful

---
+++ building-restful/src/main/java/com/example/buildingrestful/BuildingRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingRestfulApplication.class, args);
+	}
+
+}
